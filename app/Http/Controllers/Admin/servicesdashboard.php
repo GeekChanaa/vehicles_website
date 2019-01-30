683326b9349c4413a41a1180ff05da7618003dd3
chanaa <mohammedchanaa@hotmail.com>
2019-01-30 21:53:51 +0100

    smls

--- app/Http/Controllers/Admin/servicesdashboard.php
+++ app/Http/Controllers/Admin/servicesdashboard.php
@@ -4,12 +4,21 @@ namespace App\Http\Controllers\Admin;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\carwash;
+use App\workshop;
+use App\User;
 
 class servicesdashboard extends Controller
 {
     //Vars
 
+    // list of Workshops
 
+    protected $list_workshops;
+
+    // List of Carwashes
+
+    protected $list_carwashes;
 
 
     /* Statistic Vars */
@@ -28,7 +37,7 @@ class servicesdashboard extends Controller
 
     // Number of workshop owners
 
-    protected $nbr_workshop owners;
+    protected $nbr_workshop_owners;
 
     // Number of carwashes last day
 
@@ -89,6 +98,106 @@ class servicesdashboard extends Controller
     // Number of carwashes 12th last month
 
     protected $nbr_carwashes_12th_month;
+
+    // Number of workshops last day
+
+    protected $nbr_workshops_last_day;
+
+    // Number of workshops last week
+
+    protected $nbr_workshops_last_week;
+
+    // Number of workshops last month
+
+    protected $nbr_workshops_last_month;
+
+    // Number of workshops last year
+
+    protected $nbr_workshops_last_year;
+
+    // Number of workshops last 2nd month
+
+    protected $nbr_workshops_2nd_month;
+
+    // Number of workshops third last month
+
+    protected $nbr_workshops_3rd_month;
+
+    // Number of workshops forth last month
+
+    protected $nbr_workshops_4th_month;
+
+    // Number of workshops 5th last month
+
+    protected $nbr_workshops_5th_month;
+
+    // Number of workshops 6th last month
+
+    protected $nbr_workshops_6th_month;
+
+    // Number of workshops 7th last month
+
+    protected $nbr_workshops_7th_month;
+
+    // Number of workshops 8th last month
+
+    protected $nbr_workshops_8th_month;
+
+    // Number of workshops 9th last month
+
+    protected $nbr_workshops_9th_month;
+
+    // Number of workshops 10th last month
+
+    protected $nbr_workshops_10th_month;
+
+    // Number of workshops 11th last month
+
+    protected $nbr_workshops_11th_month;
+
+    // Number of workshops 12th last month
+
+    protected $nbr_workshops_12th_month;
+
+
+    public function __construct(){
+      $this->list_carwashes=carwash::all();
+      $this->$list_workshops=workshop::all();
+  $this->nbr_carwashes=carwash::all()->count();
+  $this->nbr_workshops=workshop::all()->count();
+  $this->nbr_carwash_owners=User::all()->where('rank','=','carwashowner');
+  $this->nbr_workshop_owners=User::all()->where('rank','=','workshopowner');
+  $this->nbr_carwashes_last_day=carwash::all()->where('created_at','<','DATEADD(dd, -1, GETDATE())')->count();
+  $this->nbr_carwashes_last_week=carwash::all()->where('created_at','<','DATEADD(dd, -7, GETDATE())')->count();
+  $this->nbr_carwashes_last_month=carwash::all()->where('created_at','<','DATEADD(mm, -1, GETDATE())')->count();
+  $this->nbr_carwashes_last_year=carwash::all()->where('created_at','<','DATEADD(yy, -1, GETDATE())')->count();
+  $this->nbr_carwashes_2nd_month=carwash::whereRaw('created_at < DATE_ADD(NOW(),Interval -1 month) and created_at > DATE_ADD(NOW(),Interval -2 month)')->count();
+  $this->nbr_carwashes_3rd_month=carwash::whereRaw('created_at < DATE_ADD(NOW(),Interval -2 month) and created_at > DATE_ADD(NOW(),Interval -3 month)')->count();
+  $this->nbr_carwashes_4th_month=carwash::whereRaw('created_at < DATE_ADD(NOW(),Interval -3 month) and created_at > DATE_ADD(NOW(),Interval -4 month)')->count();
+  $this->nbr_carwashes_5th_month=carwash::whereRaw('created_at < DATE_ADD(NOW(),Interval -4 month) and created_at > DATE_ADD(NOW(),Interval -5 month)')->count();
+  $this->nbr_carwashes_6th_month=carwash::whereRaw('created_at < DATE_ADD(NOW(),Interval -5 month) and created_at > DATE_ADD(NOW(),Interval -6 month)')->count();
+  $this->nbr_carwashes_7th_month=carwash::whereRaw('created_at < DATE_ADD(NOW(),Interval -6 month) and created_at > DATE_ADD(NOW(),Interval -7 month)')->count();
+  $this->nbr_carwashes_8th_month=carwash::whereRaw('created_at < DATE_ADD(NOW(),Interval -7 month) and created_at > DATE_ADD(NOW(),Interval -8 month)')->count();
+  $this->nbr_carwashes_9th_month=carwash::whereRaw('created_at < DATE_ADD(NOW(),Interval -8 month) and created_at > DATE_ADD(NOW(),Interval -9 month)')->count();
+  $this->nbr_carwashes_10th_month=carwash::whereRaw('created_at < DATE_ADD(NOW(),Interval -9 month) and created_at > DATE_ADD(NOW(),Interval -10 month)')->count();
+  $this->nbr_carwashes_11th_month=carwash::whereRaw('created_at < DATE_ADD(NOW(),Interval -10 month) and created_at > DATE_ADD(NOW(),Interval -11 month)')->count();
+  $this->nbr_carwashes_12th_month=carwash::whereRaw('created_at < DATE_ADD(NOW(),Interval -11 month) and created_at > DATE_ADD(NOW(),Interval -12 month)')->count();
+  $this->nbr_workshops_last_day=workshop::all()->where('created_at','<','DATEADD(dd, -1, GETDATE())')->count()
+  $this->nbr_workshops_last_week=workshop::all()->where('created_at','<','DATEADD(dd, -7, GETDATE())')->count()
+  $this->nbr_workshops_last_month=workshop::all()->where('created_at','<','DATEADD(mm, -1, GETDATE())')->count()
+  $this->nbr_workshops_last_year=workshop::all()->where('created_at','<','DATEADD(yy, -1, GETDATE())')->count()
+  $this->nbr_workshops_2nd_month=workshop::whereRaw('created_at < DATE_ADD(NOW(),Interval -1 month) and created_at > DATE_ADD(NOW(),Interval -2 month)')->count()
+  $this->nbr_workshops_3rd_month=workshop::whereRaw('created_at < DATE_ADD(NOW(),Interval -2 month) and created_at > DATE_ADD(NOW(),Interval -3 month)')->count()
+  $this->nbr_workshops_4th_month=workshop::whereRaw('created_at < DATE_ADD(NOW(),Interval -3 month) and created_at > DATE_ADD(NOW(),Interval -4 month)')->count()
+  $this->nbr_workshops_5th_month=workshop::whereRaw('created_at < DATE_ADD(NOW(),Interval -4 month) and created_at > DATE_ADD(NOW(),Interval -5 month)')->count()
+  $this->nbr_workshops_6th_month=workshop::whereRaw('created_at < DATE_ADD(NOW(),Interval -5 month) and created_at > DATE_ADD(NOW(),Interval -6 month)')->count()
+  $this->nbr_workshops_7th_month=workshop::whereRaw('created_at < DATE_ADD(NOW(),Interval -6 month) and created_at > DATE_ADD(NOW(),Interval -7 month)')->count()
+  $this->nbr_workshops_8th_month=workshop::whereRaw('created_at < DATE_ADD(NOW(),Interval -7 month) and created_at > DATE_ADD(NOW(),Interval -8 month)')->count()
+  $this->nbr_workshops_9th_month=workshop::whereRaw('created_at < DATE_ADD(NOW(),Interval -8 month) and created_at > DATE_ADD(NOW(),Interval -9 month)')->count()
+  $this->nbr_workshops_10th_month=workshop::whereRaw('created_at < DATE_ADD(NOW(),Interval -9 month) and created_at > DATE_ADD(NOW(),Interval -10 month)')->count()
+  $this->nbr_workshops_11th_month=workshop::whereRaw('created_at < DATE_ADD(NOW(),Interval -10 month) and created_at > DATE_ADD(NOW(),Interval -11 month)')->count()
+  $this->nbr_workshops_12th_month=workshop::whereRaw('created_at < DATE_ADD(NOW(),Interval -11 month) and created_at > DATE_ADD(NOW(),Interval -12 month)')->count()
+    }
 
 
 }
