8724099e0976e7ecf633d6149955109a620a9763
unknown <enanda.am@gmail.com>
2019-01-30 20:25:19 +0000

    my first push

--- database/migrations/2019_01_07_170829_create_new_vehicle_articles_table.php
+++ database/migrations/2019_01_07_170829_create_new_vehicle_articles_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateNewVehicleArticleTable extends Migration
+class CreateNewVehicleArticlesTable extends Migration
 {
     /**
      * Run the migrations.
