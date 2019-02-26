f95c1248574e522bc0f5c64bd07663e285317659
unknown <enanda.am@gmail.com>
2019-02-26 16:02:42 +0000

    Last changes

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -1,7 +1,12 @@
 @extends('layouts.master')
 
-@section('title','test')
+@section('title','Error 404')
 
 @section('content')
-  <img class="err-num" src="{{asset('svg/404.svg')}}" alt="">
+  <div class="container-fluid text-center err-container">
+    <img class="err-num" src="{{asset('svg/404.svg')}}" alt="">
+    <h1 class="err-title">Oops !</h1>
+    <p class="err-paragraph">We can't seem to find the page you're looking for.</p>
+    <a href="#">Return to home page</a>
+  </div>
 @endsection
