fdcc61d13df0bf5661f4612b36a5fb4118e0794c
unknown <enanda.am@gmail.com>
2019-04-04 15:06:14 +0100

    New changes

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -7,3 +7,5 @@ function changeupdate(id){
   $('.td-list-update'+id).css('display','none');
   $('.td-input-update'+id).css('display','initial');
 }
+
+// show border under todo input when it's focused
--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -19,30 +19,28 @@
 
     <script src="{{ asset('js/jquery.min.js') }}"> </script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.7.3/Chart.min.js"></script>
-    
+
 </head>
 <body>
-
-
-
-  <nav class="navbar navbar-expand-lg navbar-light bg-light">
-<a class="navbar-brand" href="#">Navbar</a>
-<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-<span class="navbar-toggler-icon"></span>
-</button>
-
-<div class="collapse navbar-collapse" id="navbarSupportedContent">
-<ul class="navbar-nav mr-auto">
-<li class="nav-item active">
-<a class="nav-link" href="#">Home</a>
-</li>
-<li class="nav-item">
-<a class="nav-link" href="#">Link</a>
-</li>
-
-</ul>
-</div>
-</nav>
+    <nav class="navbar navbar-expand navbar-primary main-nav">
+        <a class="navbar-brand" href="#">
+          <img src="{{asset('img/logo.png')}}" alt="">
+        </a>
+        <ul class="navbar-nav ml-auto">
+          <li class="nav-item">
+            <ion-icon name="create" class="align-middle"></ion-icon>
+          </li>
+          <li class="nav-item">
+            <ion-icon name="chatboxes" class="align-middle"></ion-icon>
+          </li>
+          <li class="nav-item">
+            <ion-icon name="notifications" class="align-middle"></ion-icon>
+          </li>
+          <li class="nav-item nav-avatar ">
+            <img src="{{asset('img/avatar.jpg')}}" alt="">
+          </li>
+        </ul>
+    </nav>
 
 
 
@@ -53,6 +51,7 @@
 
     <script src="{{ asset('js/bootstrap.bundle.min.js') }}"></script>
     <script src="{{ asset('js/bootstrap.js') }}"></script>
+    <script src="https://unpkg.com/ionicons@4.2.2/dist/ionicons.js"></script>
     <script src="{{ asset('js/dashboard.js') }}"></script>
 
 
