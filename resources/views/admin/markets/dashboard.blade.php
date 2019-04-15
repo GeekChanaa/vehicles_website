b4bb8b1c002df850cd8eff71360f323f116cc27d
unknown <enanda.am@gmail.com>
2019-04-15 16:34:19 +0100

    lolo

--- resources/views/admin/markets/dashboard.blade.php
+++ resources/views/admin/markets/dashboard.blade.php
@@ -9,21 +9,50 @@
 
 
 <!-- Routes to other Market Dashboards -->
+
+  <div class="container mx-auto sections row">
+    <div class="section row col-md-6 col-lg-3 ">
+      <div class="col-3 row" style="background:#519EF7">
+        <ion-icon name="car" class="col-12 align-middle"></ion-icon>
+        <h2 class="col-12">120</h2>
+      </div>
+      <a href="{{ url('/marketmoderator/newcarparts') }}" class="col-9">
+        New Carparts
+      </a>
+    </div>
+    <div class="section row col-md-6 col-lg-3 ">
+      <div class="col-3"  style="background:#488EE2">
+        <ion-icon name="quote" class="align-middle"></ion-icon>
+        <h2>120</h2>
+      </div>
+      <a href="{{ url('/marketmoderator/usedcarparts') }}" class="col-9">
+        Used Carparts
+      </a>
+    </div>
+    <div class="section row col-md-6 col-lg-3 ">
+      <div class="col-3" style="background:#3B78BD">
+        <ion-icon name="hammer" class="align-middle"></ion-icon>
+      </div>
+      <a href="{{ url('/marketmoderator/newvehicles') }}" class="col-9">
+        New Vehicles
+      </a>
+    </div>
+    <div class="section row col-md-6 col-lg-3 ">
+      <div class="col-3" style="background:#30629F">
+        <ion-icon name="school" class="align-middle"></ion-icon>
+      </div>
+      <a href="{{ url('/marketmoderator/usedvehicles') }}" class="col-9">
+        Used Vehicles
+      </a>
+    </div>
+  </div>
 <section class="bg-light">
 <div class="row">
 
-  <a href="{{ url('/marketmoderator/newcarparts') }}" class=" dashboard-route col-lg-3 btn btn-outline-danger">
-    New Carparts
-  </a>
-  <a href="{{ url('/marketmoderator/usedcarparts') }}" class=" dashboard-route col-lg-3 btn btn-outline-danger">
-    Used Carparts
-  </a>
-  <a href="{{ url('/marketmoderator/newvehicles') }}" class=" dashboard-route col-lg-3 btn btn-outline-danger">
-    New Vehicles
-  </a>
-  <a href="{{ url('/marketmoderator/usedvehicles') }}" class=" dashboard-route col-lg-3 btn btn-outline-danger">
-    Used Vehicles
-  </a>
+
+
+
+
 
 </div>
 
