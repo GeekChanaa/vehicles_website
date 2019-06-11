07b3e6643516d638f8161dcb78e78551402e1d9a
Adnane AMEZIANE <enanda.am@gmail.com>
2019-06-11 14:59:02 +0100

    styling market navbar ( Added brand logo and styled nav buttons )

--- resources/views/layouts/market.blade.php
+++ resources/views/layouts/market.blade.php
@@ -23,65 +23,56 @@
     <script src="{{ asset('js/jquery.min.js') }}"> </script>
 </head>
 <body>
-    <div id="app">
-        <nav class="navbar navbar-default navbar-static-top">
-            <div class="container">
-                <div class="navbar-header">
+      <nav class="main-nav navbar navbar-default">
+        <div class="container">
+          <!-- Navbar Header [contains both toggle button and navbar brand] -->
+          <div class="navbar-header col-3">
+            <!-- Toggle Button [handles opening navbar components on mobile screens]-->
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#exampleNavComponents" aria-expanded="false">
+              <i class="glyphicon glyphicon-align-center"></i>
+            </button>
 
-                    <!-- Collapsed Hamburger -->
-                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#app-navbar-collapse" aria-expanded="false">
-                        <span class="sr-only">Toggle Navigation</span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                    </button>
+            <!-- Navbar Brand -->
+            <a href="{{url('/')}}" class="navbar-brand">
+              <img src="{{asset('img/logo.png')}}" alt="">
+            </a>
+          </div>
 
-                    <!-- Branding Image -->
-                    <a class="navbar-brand" href="{{ url('/') }}">
+          <!-- Navbar Collapse [contains navbar components such as navbar menu and forms ] -->
+          <div class="collapse navbar-collapse col-9" id="exampleNavComponents">
 
-                    </a>
-                </div>
+            <!-- Navbar Button -->
+            @guest
+            <button type="button" class="navbar-right"><a href="{{ route('login') }}">Login</a></button>
+            <button type="button" class="navbar-right"><a href="{{ route('register') }}">Register</a></button>
+            @else
+            <li class="dropdown">
+              <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false" aria-haspopup="true" v-pre>
+                {{ Auth::user()->name }} <span class="caret"></span>
+              </a>
 
-                <div class="collapse navbar-collapse" id="app-navbar-collapse">
-                    <!-- Left Side Of Navbar -->
-                    <ul class="nav navbar-nav">
-                        &nbsp;
-                    </ul>
+              <ul class="dropdown-menu">
+                <li>
+                  <a href="{{ route('logout') }}"
+                  onclick="event.preventDefault();
+                  document.getElementById('logout-form').submit();">
+                  Logout
+                </a>
 
-                    <!-- Right Side Of Navbar -->
-                    <ul class="nav navbar-nav navbar-right">
-                        <!-- Authentication Links -->
-                        @guest
-                            <li><a href="{{ route('login') }}">Login</a></li>
-                            <li><a href="{{ route('register') }}">Register</a></li>
-                        @else
-                            <li class="dropdown">
-                                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false" aria-haspopup="true" v-pre>
-                                    {{ Auth::user()->name }} <span class="caret"></span>
-                                </a>
+                <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                  {{ csrf_field() }}
+                </form>
+              </li>
+            </ul>
+          </li>
+          @endguest
 
-                                <ul class="dropdown-menu">
-                                    <li>
-                                        <a href="{{ route('logout') }}"
-                                            onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                            Logout
-                                        </a>
+        </div>
+        </div>
+      </nav>
 
-                                        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                                            {{ csrf_field() }}
-                                        </form>
-                                    </li>
-                                </ul>
-                            </li>
-                        @endguest
-                    </ul>
-                </div>
-            </div>
-        </nav>
 
         @yield('content')
-    </div>
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}"></script>
